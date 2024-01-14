ad75d4a4cb3ec1f6e21f47fce869f6b49e96cc38
jinghong-6 <139053090+jinghong-6@users.noreply.github.com>
2024-01-14 23:12:11 +0800

    优化登录

--- src/components/Admin/Header.vue
+++ src/components/Admin/Header.vue
@@ -21,52 +21,50 @@
     <div class="Modal-view" v-if="showModal">
         <div class="overlay"></div>
         <div class="Modal-view-content">
-            <div class="Modal-view-conten-userLogin-login" v-show="loginFlag">
-                <div class="Modal-view-conten-userLogin" v-show="userLoginFlag">
-                    <div class="Modal-view-conten-userLogin-title">管理员登录</div>
-                    <div class="Modal-view-conten-userLogin-account">
-                        <div>
-                            <svg t="1689497299306" class="Modal-view-conten-userLogin-account-icon" viewBox="0 0 1024 1024"
-                                version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2281" width="200" height="200">
-                                <path
-                                    d="M511.82655 556.277436c-135.928832 0-246.477624-110.546745-246.477624-246.477624S375.896694 63.322188 511.82655 63.322188c135.930879 0 246.477624 110.546745 246.477624 246.477624S647.757428 556.277436 511.82655 556.277436zM511.82655 108.135836c-111.202685 0-201.662953 90.481757-201.662953 201.662953s90.460268 201.662953 201.662953 201.662953c111.159706 0 201.662953-90.481757 201.662953-201.662953S622.986255 108.135836 511.82655 108.135836z"
-                                    p-id="2282" fill="#8a8a8a"></path>
-                                <path
-                                    d="M870.340853 959.603341 153.314293 959.603341c-37.068244 0-67.220984-30.15274-67.220984-67.220984 0-3.061732 0.656963-75.535347 54.923914-147.879002 31.596625-42.143842 74.835406-75.578326 128.576377-99.474572 65.559135-29.143761 147.091056-43.937698 242.23295-43.937698 95.14394 0 176.630836 14.792914 242.276952 43.937698 53.741995 23.896246 96.980775 57.33073 128.577401 99.474572 54.266951 72.342632 54.880935 144.81727 54.880935 147.879002C937.561837 929.4506 907.409097 959.603341 870.340853 959.603341zM511.82655 645.905756c-156.280346 0-271.553743 42.712801-333.436138 123.501802-46.38954 60.567447-47.439452 122.449843-47.483454 123.062803 0 12.254092 10.021238 22.320355 22.406313 22.320355l717.02656 0c12.341073 0 22.407336-10.066263 22.407336-22.407336 0-0.524956-1.050935-62.406329-47.440475-122.974799C783.33629 688.618557 668.063917 645.905756 511.82655 645.905756z"
-                                    p-id="2283" fill="#8a8a8a"></path>
-                            </svg>
-                        </div>
-                        <div class="Modal-view-conten-userLogin-account-div">
-                            <label ref="teleUserLabel">telephone</label>
-                            <input type="text" @focus="teleUserFocus" @blur="teleUserBlur" v-model="teleUserRef"
-                                ref="teleUserInput">
-                        </div>
+            <div class="Modal-view-conten-userLogin" v-show="userLoginFlag">
+                <div class="Modal-view-conten-userLogin-title">管理员登录</div>
+                <div class="Modal-view-conten-userLogin-account">
+                    <div>
+                        <svg t="1689497299306" class="Modal-view-conten-userLogin-account-icon" viewBox="0 0 1024 1024"
+                            version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2281" width="200" height="200">
+                            <path
+                                d="M511.82655 556.277436c-135.928832 0-246.477624-110.546745-246.477624-246.477624S375.896694 63.322188 511.82655 63.322188c135.930879 0 246.477624 110.546745 246.477624 246.477624S647.757428 556.277436 511.82655 556.277436zM511.82655 108.135836c-111.202685 0-201.662953 90.481757-201.662953 201.662953s90.460268 201.662953 201.662953 201.662953c111.159706 0 201.662953-90.481757 201.662953-201.662953S622.986255 108.135836 511.82655 108.135836z"
+                                p-id="2282" fill="#8a8a8a"></path>
+                            <path
+                                d="M870.340853 959.603341 153.314293 959.603341c-37.068244 0-67.220984-30.15274-67.220984-67.220984 0-3.061732 0.656963-75.535347 54.923914-147.879002 31.596625-42.143842 74.835406-75.578326 128.576377-99.474572 65.559135-29.143761 147.091056-43.937698 242.23295-43.937698 95.14394 0 176.630836 14.792914 242.276952 43.937698 53.741995 23.896246 96.980775 57.33073 128.577401 99.474572 54.266951 72.342632 54.880935 144.81727 54.880935 147.879002C937.561837 929.4506 907.409097 959.603341 870.340853 959.603341zM511.82655 645.905756c-156.280346 0-271.553743 42.712801-333.436138 123.501802-46.38954 60.567447-47.439452 122.449843-47.483454 123.062803 0 12.254092 10.021238 22.320355 22.406313 22.320355l717.02656 0c12.341073 0 22.407336-10.066263 22.407336-22.407336 0-0.524956-1.050935-62.406329-47.440475-122.974799C783.33629 688.618557 668.063917 645.905756 511.82655 645.905756z"
+                                p-id="2283" fill="#8a8a8a"></path>
+                        </svg>
                     </div>
-                    <div class="Modal-view-conten-userLogin-pwd">
-                        <div>
-                            <svg t="1689497374247" class="Modal-view-conten-userLogin-pwd-icon" viewBox="0 0 1024 1024"
-                                version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3893" width="200" height="200">
-                                <path
-                                    d="M534.102928 715.684621c-12.791046 0-23.126212-10.335165-23.126212-23.126212l0-41.852303c-25.377436-9.516538-42.773259-34.280004-42.773259-61.704007 0-36.326571 29.572899-65.89947 65.89947-65.89947 36.326571 0 65.89947 29.572899 65.89947 65.89947 0 27.424003-17.395823 52.187469-42.773259 61.704007L557.22914 692.558409C557.22914 705.247127 546.893974 715.684621 534.102928 715.684621z"
-                                    p-id="3894" fill="#8a8a8a"></path>
-                                <path
-                                    d="M275.519137 907.652643c-41.238333 0-74.904367-35.507944-74.904367-79.202158L200.61477 444.002798l667.078645 0 0 384.550015c0 43.694214-33.563705 79.202158-74.904367 79.202158L275.519137 907.652643 275.519137 907.652643zM240.420506 807.780154c0 35.405616 22.205256 59.145798 55.25732 59.145798l476.850205 0c33.052064 0 55.25732-23.740182 55.25732-59.145798l0-322.232038L240.420506 485.548116 240.420506 807.780154z"
-                                    p-id="3895" fill="#8a8a8a"></path>
-                                <path
-                                    d="M755.336864 449.52853 755.336864 326.734486c0-126.17088-93.52813-217.754772-222.359548-217.754772-128.831418 0-222.359548 91.583891-222.359548 217.754772l0 122.691716L268.151494 449.426202 268.151494 323.562306c0-68.662336 27.526332-133.231538 77.667233-181.735185 50.038573-48.503647 116.449685-75.211352 187.158589-75.211352s137.222344 26.707705 187.158589 75.211352c50.038573 48.503647 77.667233 113.072849 77.667233 181.735185l0 125.966224L755.336864 449.52853 755.336864 449.52853z"
-                                    p-id="3896" fill="#8a8a8a"></path>
-                            </svg>
-                        </div>
-                        <div class="Modal-view-conten-userLogin-pwd-div">
-                            <label ref="pwdUserLabel">password</label>
-                            <input type="password" @focus="pwdUserFocus" @blur="pwdUserBlur" v-model="pwdUserRef">
-                        </div>
+                    <div class="Modal-view-conten-userLogin-account-div">
+                        <label ref="teleUserLabel">telephone</label>
+                        <input type="text" @focus="teleUserFocus" @blur="teleUserBlur" v-model="teleUserRef"
+                            ref="teleUserInput">
                     </div>
-                    <div class="Modal-view-conten-userLogin-text">欢迎回来</div>
-                    <div class="Modal-view-conten-userLogin-loginButton">
-                        <div @click="adminLogin">
-                            登录
-                        </div>
+                </div>
+                <div class="Modal-view-conten-userLogin-pwd">
+                    <div>
+                        <svg t="1689497374247" class="Modal-view-conten-userLogin-pwd-icon" viewBox="0 0 1024 1024"
+                            version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3893" width="200" height="200">
+                            <path
+                                d="M534.102928 715.684621c-12.791046 0-23.126212-10.335165-23.126212-23.126212l0-41.852303c-25.377436-9.516538-42.773259-34.280004-42.773259-61.704007 0-36.326571 29.572899-65.89947 65.89947-65.89947 36.326571 0 65.89947 29.572899 65.89947 65.89947 0 27.424003-17.395823 52.187469-42.773259 61.704007L557.22914 692.558409C557.22914 705.247127 546.893974 715.684621 534.102928 715.684621z"
+                                p-id="3894" fill="#8a8a8a"></path>
+                            <path
+                                d="M275.519137 907.652643c-41.238333 0-74.904367-35.507944-74.904367-79.202158L200.61477 444.002798l667.078645 0 0 384.550015c0 43.694214-33.563705 79.202158-74.904367 79.202158L275.519137 907.652643 275.519137 907.652643zM240.420506 807.780154c0 35.405616 22.205256 59.145798 55.25732 59.145798l476.850205 0c33.052064 0 55.25732-23.740182 55.25732-59.145798l0-322.232038L240.420506 485.548116 240.420506 807.780154z"
+                                p-id="3895" fill="#8a8a8a"></path>
+                            <path
+                                d="M755.336864 449.52853 755.336864 326.734486c0-126.17088-93.52813-217.754772-222.359548-217.754772-128.831418 0-222.359548 91.583891-222.359548 217.754772l0 122.691716L268.151494 449.426202 268.151494 323.562306c0-68.662336 27.526332-133.231538 77.667233-181.735185 50.038573-48.503647 116.449685-75.211352 187.158589-75.211352s137.222344 26.707705 187.158589 75.211352c50.038573 48.503647 77.667233 113.072849 77.667233 181.735185l0 125.966224L755.336864 449.52853 755.336864 449.52853z"
+                                p-id="3896" fill="#8a8a8a"></path>
+                        </svg>
+                    </div>
+                    <div class="Modal-view-conten-userLogin-pwd-div">
+                        <label ref="pwdUserLabel">password</label>
+                        <input type="password" @focus="pwdUserFocus" @blur="pwdUserBlur" v-model="pwdUserRef">
+                    </div>
+                </div>
+                <div class="Modal-view-conten-userLogin-text">欢迎回来</div>
+                <div class="Modal-view-conten-userLogin-loginButton">
+                    <div @click="adminLogin">
+                        登录
                     </div>
                 </div>
             </div>
@@ -117,7 +115,6 @@ import useStore from '@/utils/adminInfo';
 let userInfoStore = useStore()
 
 let showModal = ref(false)
-let loginFlag = ref(true)
 let userLoginFlag = ref(true)
 let userInfoFlag = ref(false)
 
@@ -135,14 +132,20 @@ let loginFail = ref(false)
 let userName = ref()
 
 onMounted(() => {
+    let token = localStorage.getItem("aatt")
+    if (token != undefined && token != "") {
+        showModal.value = false;
+    } else {
+        showModal.value = true;
+    }
 })
 
 function adminLogin() {
     let data = {
-        Account : teleUserRef.value,
-        pwd:pwdUserRef.value
+        Account: teleUserRef.value,
+        pwd: pwdUserRef.value
     }
-    
+
     postAdminLogin(data).then(
         res => {
             if (res.status == "200") {
@@ -172,15 +175,12 @@ function loginAdminDetail(adminDetail) {
     userInfoStore.admin_name = adminDetail.admin.admin_name
     userInfoStore.money = adminDetail.admin.money
 
+    localStorage.setItem('aatt', adminDetail.token);
+
     userName.value = userInfoStore.admin_name
     userInfoFlag.value = true
-    closeModal()
-}
-
-// 打开登录模态框
-function openLoginModal() {
-    showModal.value = true;
-    loginFlag.value = true
+    
+    showModal.value = false
 }
 
 // 用户账号获得焦点
