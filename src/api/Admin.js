05df82547269ec88bfbdcc9f8f89a2678250f353
jinghong-6 <139053090+jinghong-6@users.noreply.github.com>
2024-02-07 15:30:27 +0800

    新增管理员查看通知

--- src/api/Admin.js
+++ src/api/Admin.js
@@ -79,3 +79,38 @@ export function changeLandlordStatus(data,token) {
         data:data
     })
 }
+
+export function getAdminAlert(token) {
+    return request({
+        url: '/adminAlert',
+        method: 'post',
+        headers: {
+            'Authorization': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    })
+}
+
+export function getAdminAlertById(data,token) {
+    return request({
+        url: '/adminAlert/detail',
+        method: 'post',
+        headers: {
+            'Authorization': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        data:data
+    })
+}
+
+export function readAdminAlertByAlertId(data,token) {
+    return request({
+        url: '/adminAlert/read',
+        method: 'post',
+        headers: {
+            'Authorization': token,
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        data:data
+    })
+}
